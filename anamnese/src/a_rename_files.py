2cfe62dea35d41e29fae68655175f347a8e05832
Ângelo Benetti <angelobbenetti@gmail.com>
2025-11-11 11:51:19 -0300

    :sparkles: feat (rename_files): get_person_code and add logs

--- anamnese/src/a_rename_files.py
+++ anamnese/src/a_rename_files.py
@@ -54,14 +54,16 @@ def rename_canon_files(files_dict, camera_name):
     del files_dict[f"canon_{camera_name}_new"]
     return files_dict
 
-def get_person_code():
+def get_person_code(config_general):
     """Prompt user for person code."""
+    reverse_person_codes = {v: k for k, v in config_general["pessoa"].items()}
     pessoa = None
-    while pessoa not in ["p001", "p002", "p003"]:
+    while pessoa not in list(reverse_person_codes):
         pessoa = input(
-            "Qual pessoa está gravando (Isabela -> p001, Thyago -> p002, Natalia -> p003): "
+            f"Qual pessoa está gravando {list(reverse_person_codes)}: "
         )
-    return pessoa
+        print(f"Pessoa selecionada: {pessoa}")
+    return reverse_person_codes[pessoa]
 
 def get_anamnese_dataframe(config_anamnese):
     """Load the anamnese sentences dataframe."""
@@ -71,6 +73,13 @@ def get_anamnese_dataframe(config_anamnese):
     for sub_df in dfs.values():
         df = pd.concat([df, sub_df[columns]], ignore_index=True)
     return df
+
+def check_selected_files_df(df_files):
+    for index, row in df_files[~df_files.is_selected].iterrows():
+        upper_index = index-10 if index-10 >=0 else 0
+        lower_index = index+10 if index+10 < len(df_files) else len(df_files)-1
+        context = df_files.iloc[upper_index:lower_index+1]
+        print(context)
 
 def get_select_files_dataframe(files):
     files_df = pd.DataFrame(
@@ -86,46 +95,51 @@ def get_select_files_dataframe(files):
     for index, row in files_df.iterrows():
         file_id = row['file_id']
         if file_id[-1].isalpha():
-            files_df.at[index-1, 'is_selected'] = False
+            original_file_id = file_id[:-1]
+            if original_file_id in files_df['file_id'].values:
+                files_df.loc[files_df['file_id'] == original_file_id, 'is_selected'] = False
 
     return files_df
 
+def add_logs(old_file_name, new_file_name, log_file="anamnese/logs/rename_log.txt"):
+    """Append rename actions to a log file."""
+    date = pd.Timestamp.now().strftime("%Y-%m-%d %H:%M:%S")
+    with open(log_file, "a") as log:
+        log.write(f"{date};{old_file_name};{new_file_name}\n")
+
 def rename_files_to_standard(files_dict, config_anamnese, config_general):
     """Rename files to a standardized format."""
-    pessoa = get_person_code()
-    df = get_anamnese_dataframe(config_anamnese)
+    person = get_person_code(config_general)
+    df_sentences = get_anamnese_dataframe(config_anamnese)
     for camera, files in tqdm(files_dict.items()):
-        df = get_select_files_dataframe(files)
-
-        for file_id, file_path in files:
-
-            import ipdb
-            ipdb.set_trace()
+        camera_id = config_general["camera"][camera]
+        df_files = get_select_files_dataframe(files)
+        for file_id, file_path, is_selected in df_files.itertuples(index=False):
             take="t000"
-            file_id = Path(file).stem.split(".")[0]
+            file_id = Path(file_path).stem.split(".")[0]
 
             if file_id[-1].isalpha():
                 file_id = file_id[:-1]
 
-            matching_id = df[df["ID"] == file_id]["ID Sentenca"].squeeze()
+            matching_id = df_sentences[df_sentences["ID"] == file_id]["ID Sentenca"].squeeze()
 
             if matching_id:
-                root_folder = Path(config_anamnese["root_folder"]) / matching_id
+                root_folder_str = config_anamnese["root_folder"] if is_selected else config_anamnese["root_folder_errors"]
+                root_folder = Path(root_folder_str) / matching_id
                 root_folder.mkdir(parents=True, exist_ok=True)
-                new_name = f"{matching_id}_{pessoa}_{camera}_{take}.mp4"
+                new_name = f"{matching_id}_{person}_{camera_id}_{take}.mp4"
                 new_file_path = root_folder / new_name
 
                 while new_file_path.exists():
                     take = f"t{int(take[1:]) + 1:03d}"
-                    camera_id = config_general["camera"][camera]
-                    new_name = f"{matching_id}_{pessoa}_{camera_id}_{take}.mp4"
+                    new_name = f"{matching_id}_{person}_{camera_id}_{take}.mp4"
                     new_file_path = root_folder / new_name
 
-                os.rename(file, new_file_path)
+                os.rename(file_path, new_file_path)
+                add_logs(file_path, new_file_path)
+
             else:
                 raise ValueError(f"No matching sentence ID found for file ID: {file_id}")
-    import ipdb
-    ipdb.set_trace()
 
 def main(config_anamnese, config_general):
     """Main function to orchestrate file renaming."""
